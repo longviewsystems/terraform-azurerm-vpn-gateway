94fdc6f98cba3ea719b28b80a89b4256fb0cb832
Giridhar Kolli <Giridhar.Kolli@lvs1.com>
2023-07-19 12:52:57 -0700

    updates

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_public_ip" "pip_gw" {
   resource_group_name = var.resource_group_name
   allocation_method   = var.vpn_public_ip_allocation_method
   sku                 = var.vpn_public_ip_sku
-  zones               = ""
+  
 }
 
 
